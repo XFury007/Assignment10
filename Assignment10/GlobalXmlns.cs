5c4f9e3fcdcfd97bacfa440ac5f8f9932021f533
XFury007 <grevematthew@gmail.com>
2025-08-26 13:42:33 +0200

    model setup

---
+++ Assignment10/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Assignment10")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Assignment10.Pages")]
